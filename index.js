cd8972305a0e505070f1e5fae8c0200cd9533f74
endless-runner-007 <it.961205@hotmail.com>
2025-06-19 00:33:58 -0500

    solve problem

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -11,8 +13,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
